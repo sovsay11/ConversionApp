c2d2689da6c07621859849a5bb5fc5ba584987d8
Noah <sovannarasay11@gmail.com>
2022-05-12 21:03:57 -0600

    Implements primary logic

--- ConversionApp/ConversionApp/MainPage.xaml.cs
+++ ConversionApp/ConversionApp/MainPage.xaml.cs
@@ -8,15 +8,30 @@ using Xamarin.Forms;
 
 namespace ConversionApp
 {
-  public partial class MainPage : ContentPage
-  {
-    // stuff to go over:
-    // Basics of XAML
-    // Basics of C# and tying stuff together
-    // Go over page navigation later?
-    public MainPage()
+    public partial class MainPage : ContentPage
     {
-      InitializeComponent();
+        // stuff to go over:
+        // Basics of XAML
+        // Basics of C# and tying stuff together
+        // Go over page navigation later?
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnConvertLength_Clicked(object sender, EventArgs e)
+        {
+            double length = double.Parse(EntLengthValue.Text);
+            double convertedLength = length * 2.9;
+            LblConvertedLength.Text = convertedLength.ToString();
+        }
+
+        private void BtnConvertTemp_Clicked(object sender, EventArgs e)
+        {
+            // figure out how to round numbers...
+            double temp = double.Parse(EntTempValue.Text);
+            double convertedTemp = (temp - 32) * (5.0 / 9.0);
+            LblConvertedTemp.Text = convertedTemp.ToString("n2");
+        }
     }
-  }
 }
