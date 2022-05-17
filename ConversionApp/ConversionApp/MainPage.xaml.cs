c7f71b8840bb75a8cffa5f1fed2025a5c7169f96
Noah <sovannarasay11@gmail.com>
2022-05-17 16:44:48 -0600

    Minor changes

--- ConversionApp/ConversionApp/MainPage.xaml.cs
+++ ConversionApp/ConversionApp/MainPage.xaml.cs
@@ -8,30 +8,47 @@ using Xamarin.Forms;
 
 namespace ConversionApp
 {
-    public partial class MainPage : ContentPage
+  public partial class MainPage : ContentPage
+  {
+    // stuff to go over:
+    /* Basics of XAML
+     * - Layouts: grid, stack, scroll
+     * - Controls: Buttons, labels, entries, frames, pickers
+     * - Events: creating and triggering events
+     */
+
+    /// <summary>
+    /// Main method that ties the UI and C# code together
+    /// </summary>
+    public MainPage()
     {
-        // stuff to go over:
-        // Basics of XAML
-        // Basics of C# and tying stuff together
-        // Go over page navigation later?
-        public MainPage()
-        {
-            InitializeComponent();
-        }
-
-        private void BtnConvertLength_Clicked(object sender, EventArgs e)
-        {
-            double length = double.Parse(EntLengthValue.Text);
-            double convertedLength = length * 2.9;
-            LblConvertedLength.Text = convertedLength.ToString();
-        }
-
-        private void BtnConvertTemp_Clicked(object sender, EventArgs e)
-        {
-            // figure out how to round numbers...
-            double temp = double.Parse(EntTempValue.Text);
-            double convertedTemp = (temp - 32) * (5.0 / 9.0);
-            LblConvertedTemp.Text = convertedTemp.ToString("n2");
-        }
+      InitializeComponent();
     }
+
+    /// <summary>
+    /// Convert the length
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private void BtnConvertLength_Clicked(object sender, EventArgs e)
+    {
+      // Should go over data types...
+      double length = double.Parse(EntLengthValue.Text);
+      double convertedLength = length * 2.9;
+      LblConvertedLength.Text = convertedLength.ToString();
+    }
+
+    /// <summary>
+    /// Convert the temperature
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private void BtnConvertTemp_Clicked(object sender, EventArgs e)
+    {
+      // figure out how to round numbers...
+      double temp = double.Parse(EntTempValue.Text);
+      double convertedTemp = (temp - 32) * (5.0 / 9.0);
+      LblConvertedTemp.Text = convertedTemp.ToString("n2");
+    }
+  }
 }
